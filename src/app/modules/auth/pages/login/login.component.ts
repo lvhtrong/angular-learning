94b6ea6ffc4855f8701760235d5a5031cc3e7b07
lvhtrong <35479106+lvhtrong@users.noreply.github.com>
2020-05-15 16:40:59 +0700

    Login - Implement UI

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  username = '';
+  password = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onUsernameChange(value: string) {
+    this.username = value;
   }
 
+  onPasswordChange(value: string) {
+    this.password = value;
+  }
+
+  onSubmitClick() {
+    console.log({
+      username: this.username,
+      password: this.password,
+    });
+    // TODO: call api
+  }
 }
