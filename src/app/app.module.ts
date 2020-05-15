2f40416d63c8d010c9732027444434bd1ea030fc
lvhtrong <35479106+lvhtrong@users.noreply.github.com>
2020-05-15 16:40:44 +0700

    Add bootstrap library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CoreModule } from '@core/core.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreModule } from '@core/core.module';
 import { AuthModule } from './modules/auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, AuthModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, AuthModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
